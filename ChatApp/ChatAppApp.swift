94b54c9ef3218c16cad7f70329fe86447baea600
rubiya1 <rubiya1090@gmail.com>
2024-11-25 10:47:15 +0530

    Initial Commit

---
+++ ChatApp/ChatAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChatAppApp.swift
+//  ChatApp
+//
+//  Created by Rubiya Faniband on 25/11/24.
+//
+
+import SwiftUI
+
+@main
+struct ChatAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ChatAppTests/ChatAppTests.swift
@@ -0,0 +1,16 @@
+//
+//  ChatAppTests.swift
+//  ChatAppTests
+//
+//  Created by Rubiya Faniband on 25/11/24.
+//
+
+import Testing
+
+struct ChatAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
